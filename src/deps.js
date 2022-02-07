b900387c6367e85a279d9080853096a2f3c3815f
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-02-07 17:33:25 +0100

    feat(bundle): add file

--- src/deps.js
+++ src/deps.js
@@ -1,11 +1,7 @@
 // Copyright 2021-2022 stephendltg. All rights reserved. ISC license.
 
 /**
-<<<<<<< HEAD
  * deps.js
-=======
- * deps.ts
->>>>>>> 233087a44a10a83d54b7663ef0456d736aa9a676
  *
  * This module re-exports the required methods from the dependant remote Ramda module.
  */
